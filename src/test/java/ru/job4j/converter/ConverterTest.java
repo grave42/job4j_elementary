38c39de2460024bc67438b5f095c719b84a5e623
filipp.kucherov <filipp.kucherov@r1.team>
2023-05-09 17:12:14 +0700

    1. Модульные тесты. [#159435]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -15,10 +15,6 @@ public class ConverterTest {
     }
 
     @Test
-    public void whenConvert200RblThen3dot3333Dlr() {
-    }
-
-    @Test
     public void whenConvert180RblThen3Dlr() {
         float in = 180;
         float expected = 3;
