c80c5903f734ef59a058c1bd3a62b5ea0ec3f1f1
filipp.kucherov <filipp.kucherov@r1.team>
2023-05-13 19:08:07 +0700

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Vasya";
+        names[1] = "Petya";
+        names[2] = "Vovka";
+        names[3] = "Filipp";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
