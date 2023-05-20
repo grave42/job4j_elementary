695353687b240595589e0239a663131b9e119a3f
filipp.kucherov <filipp.kucherov@r1.team>
2023-05-20 21:49:43 +0700

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,27 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    String name;
+    private int sizeOfEmpty = 10;
+
+    public String surname;
+
+    public static final String newValue = "";
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f) {
+
+    }
+
+    void broken() {
+    }
+
+    void echo() {
+    }
+
+}
